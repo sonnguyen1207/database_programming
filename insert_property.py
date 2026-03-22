03cd87def4c41dea68a69c19ddc1d20bd0b55985
Son Nguyen <lamson1207@gmail.com>
2026-03-22 21:56:30 +0200

    added functions: delete property by name and by code

--- insert_property.py
+++ insert_property.py
@@ -19,10 +19,54 @@ def add_property(code, name, location):
     print("✅ Added")
 
 
-def delete_property(property_id):
+def delete_property_by_id(property_id):
     cursor.execute("DELETE FROM properties WHERE id = ?", (property_id,))
     conn.commit()
     print("🗑 Deleted")
+
+
+def delete_property_by_code(property_code):
+    # code is unique
+    # need to delete children (electricity) before delete parent (property)
+    cursor.execute("SELECT id FROM properties WHERE code = ?",
+                   (property_code,))
+    # delete child rows
+    property = cursor.fetchone()
+    if not property:
+        print("Property not found")
+        return
+    property_id = property[0]
+    cursor.execute(
+        "DELETE FROM electricity WHERE property = ?", (property_id,))
+    # delete parent
+    cursor.execute("DELETE FROM properties WHERE id = ?", (property_id,))
+    conn.commit()
+    print("🗑 Deleted")
+
+
+def delete_property_by_name(property_name):
+    # property is may still used in another table(electricity), need to delete children first then parent
+    # property name may have more than 1 property, need to delete all of their children before delete them
+    # get property id
+    cursor.execute("SELECT id FROM properties WHERE name = ?",
+                   (property_name,))
+
+    for property in cursor.fetchall():
+        if not property:
+            print("Property not found")
+            return
+
+        property_id = property[0]
+        # print(property_id)
+        # The mycursor.execute(sql, adr) method expects the second argument to be a sequence(like a list or a tuple), not a single string.
+        # delete child rows FIRST
+        cursor.execute(
+            "DELETE FROM electricity WHERE property = ?", (property_id,))
+        # then delete parent
+        cursor.execute("DELETE FROM properties WHERE id = ?", (property_id,))
+
+    conn.commit()
+    print("🗑 Deleted successfully")
 
 
 # --- MENU ---
@@ -40,8 +84,11 @@ while True:
         add_property(code, name, location)
 
     elif choice == "3":
-        pid = int(input("ID to delete: "))
-        delete_property(pid)
+        # pid = int(input("ID to delete: "))
+        # pname = input("Name to delete: ")
+        # delete_property_by_name(pname)
+        pcode = input("Code to delete: ")
+        delete_property_by_code(pcode)
 
     elif choice == "4":
         break
